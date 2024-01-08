4b302be9097aded933fa0104ba69e962d9127927
Murodbey <abdullayevmurod@gmail.com>
2024-01-08 17:22:20 -0600

    task1

---
+++ week6/task1/home.js
@@ -0,0 +1,4 @@
+// Name | Language | Type | Release Date
+// Create a table with the columns above and fill that table with the response data from the API above.
+// 1) create separate arrays for each column (Name, language, type and release date)
+// 2) after populating separate arrays with data from api, we need to create the table rows iterating through all arrays
