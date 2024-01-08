0cf1473d64cbc1f2c73e4eacc1eb26cf4232243b
Murodbey <abdullayevmurod@gmail.com>
2024-01-08 17:23:25 -0600

    tableapi branch added

--- week6/task1/home.js
+++ week6/task1/home.js
@@ -2,3 +2,4 @@
 // Create a table with the columns above and fill that table with the response data from the API above.
 // 1) create separate arrays for each column (Name, language, type and release date)
 // 2) after populating separate arrays with data from api, we need to create the table rows iterating through all arrays
+//https://api.tvmaze.com/search/shows?q=police
